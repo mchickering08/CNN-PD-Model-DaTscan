73d2439023b10130624cdda46a2f154c4abc8eb9
mchickering08 <72114543+mchickering08@users.noreply.github.com>
2025-11-13 21:39:38 -0500

    Update preprocessing pseudocode

--- DaTSCAN Image Preprocessing Pipeline for PD Classification Pseudocode.py
+++ DaTSCAN Image Preprocessing Pipeline for PD Classification Pseudocode.py
@@ -1,3 +1,7 @@
 import os #working w/ file paths and directory listings
 import numpy as np #numerical arrays and math operations
-import pandas as p
+import pandas as pd #reading metadata tables (CSV)
+import pydicom #loading DICOM med imaging files !
+import SimpleITK as sitk #for image resampling and med image processing !!
+import cv2 #OpenCV for resizing images and basic image operations !!
+
